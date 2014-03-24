67155ce699f6869c183e653d254eedf1962628e1
Gaston Sanchez <gaston.stat@gmail.com>
2014-03-23 20:44:29 -0700

    version 0.1.8

--- R/arcplot.r
+++ R/arcplot.r
@@ -10,14 +10,22 @@
 #' the function \code{points}. In turn, node labels are displayed
 #' through the function \code{mtext}.
 #' 
+#' When \code{ordering} is provided in numeric format and node labels are 
+#' strings, the labels are alphabetically ordered first, and then nodes are 
+#' sorted according to the provided \code{ordering}.
+#' 
+#' If \code{ordering} is provided in string format, the node labels must be 
+#' strings as well. The nodes will be sorted according to \code{ordering}.
+#' 
 #' @param edgelist basically a two-column matrix with edges 
 #' (see \code{\link{graph}})
 #' @param sorted logical to indicate if nodes should be sorted 
 #' (default \code{FALSE})
 #' @param decreasing logical to indicate type of sorting 
 #' (used only when \code{sorted=TRUE})
-#' @param ordering optional numeric vector providing the ordering of nodes
-#' (when provided, this parameter overrides \code{sorted=TRUE})
+#' @param ordering optional numeric or string vector providing the 
+#' ordering of nodes. When provided, this parameter overrides 
+#' \code{sorted=TRUE}). See the details section for more information.
 #' @param horizontal logical indicating whether to plot 
 #' in horizontal orientation
 #' @param col.arcs color for the arcs (default \code{"gray50"})
@@ -60,6 +68,28 @@
 #' @examples
 #'
 #'  \dontrun{
+#'  # create an edgelist
+#'  un_graphe <- rbind(
+#'  c("fromage", "pain"), 
+#'  c("pain", "vin"),
+#'  c("vin", "biere"),
+#'  c("cidre", "biere"),
+#'  c("foie", "fromage"),
+#'  c("pain", "foie"))
+#'  
+#'  # deafult arcplot
+#'  arcplot(un_graphe)
+#'  # vertical display
+#'  arcplot(un_graphe, horizontal=FALSE)
+#'  # nodes sorted alphabetically (increasing)
+#'  arcplot(un_graphe, sorted=TRUE)
+#'  # nodes sorted alphabetically (decreasing)
+#'  arcplot(un_graphe, sorted=TRUE, decreasing = TRUE)
+#'  # provided order for nodes
+#'  new_order = c("vin", "biere", "cidre", "fromage", "foie", "pain")
+#'  arcplot(un_graphe, ordering = new_order)
+#'  
+#'  
 #'  # generate graphs
 #'  ring_graph = graph.ring(10)
 #'  star_graph = graph.star(10, mode="out")
@@ -98,17 +128,14 @@ arcplot <- function(
   # ======================================================
   # Checking arguments
   # ======================================================
-  # make sure edgelist is a two-column matrix
+  # edgelist as a two-column matrix
   if (!is.matrix(edgelist) || ncol(edgelist) != 2)
     stop("\nSorry, 'edgelist' must be a two column matrix")
-  # how many edges
+
   num_edges = nrow(edgelist)
   # get nodes (this could be numeric or character)
   nodes = unique(as.vector(t(edgelist)))
-  # how many nodes
   num_nodes = length(nodes)
-  # node numeric sequence
-  nums = seq_along(nodes)
   # check labels (i.e. node names)
   if (!is.null(labels))
   {
@@ -121,19 +148,30 @@ arcplot <- function(
   aux_ord = 1:num_nodes  
   # If sorted is required, ennumerate nodes
   if (sorted) {
-    ord_nodes = order(nodes, decreasing=decreasing)
-    nodes = nodes[ord_nodes]
-    nums = nums[ord_nodes]
-    labels = labels[ord_nodes]
+    ordered_nodes = order(nodes, decreasing = decreasing)
+    nodes = nodes[ordered_nodes]
+    labels = labels[ordered_nodes]
     # auxiliar order
-    aux_ord = ord_nodes
+    aux_ord = ordered_nodes
   }
   # If ordering is provided, re-ennumerate nodes
-  if (!is.null(ordering)) {
-    if (length(ordering) != num_nodes)
-      stop("\nLength of 'ordering' differs from number of nodes")
+  if (!is.null(ordering)) 
+  {
+    if (length(ordering) != num_nodes) {
+      stop("\nLength of 'ordering' differs from number of nodes")      
+    }
+    
+    if (is.character(ordering)) {
+      # make sure labels contains elements in ordering
+      unmatched_ordering <- !(ordering %in% labels)
+      if (any(unmatched_ordering)) {
+        undetected = ordering[unmatched_ordering]
+        stop(sprintf("\nUnrecognized values in ordering: '%s'", undetected))
+      }
+      ordering = match(ordering, labels)
+    }
+    
     nodes = nodes[ordering]
-    nums = nums[ordering]
     labels = labels[ordering]
     # auxiliar order
     aux_ord = ordering
@@ -153,33 +191,39 @@ arcplot <- function(
     lty = rep(lty, length=num_edges)
 
   # pch symbol of nodes
-  if (length(pch.nodes) != num_nodes) 
-    pch.nodes = rep(pch.nodes, length=num_nodes)
+  if (length(pch.nodes) != num_nodes) {
+    pch.nodes = rep(pch.nodes, length = num_nodes)    
+  }
   pch.nodes = pch.nodes[aux_ord]
   # cex of nodes
-  if (length(cex.nodes) != num_nodes) 
-    cex.nodes = rep(cex.nodes, length=num_nodes)
+  if (length(cex.nodes) != num_nodes) {
+    cex.nodes = rep(cex.nodes, length = num_nodes)    
+  }
   cex.nodes = cex.nodes[aux_ord]
   # color of nodes
-  if (length(col.nodes) != num_nodes) 
-    col.nodes = rep(col.nodes, length=num_nodes)
+  if (length(col.nodes) != num_nodes) {
+    col.nodes = rep(col.nodes, length = num_nodes)    
+  }
   col.nodes = col.nodes[aux_ord]
   # bg of nodes
-  if (length(bg.nodes) != num_nodes) 
-    bg.nodes = rep(bg.nodes, length=num_nodes)
+  if (length(bg.nodes) != num_nodes) {
+    bg.nodes = rep(bg.nodes, length = num_nodes)    
+  }
   bg.nodes = bg.nodes[aux_ord]
   # line widths of nodes
-  if (length(lwd.nodes) != num_nodes) 
-    lwd.nodes = rep(lwd.nodes, length=num_nodes)
+  if (length(lwd.nodes) != num_nodes) {
+    lwd.nodes = rep(lwd.nodes, length = num_nodes)    
+  }
   lwd.nodes = lwd.nodes[aux_ord]
-    
   # color of labels
-  if (length(col.labels) != num_nodes) 
-    col.labels = rep(col.labels, length=num_nodes)
+  if (length(col.labels) != num_nodes) {
+    col.labels = rep(col.labels, length = num_nodes)    
+  } 
   col.labels = col.labels[aux_ord]
   # cex of labels
-  if (length(cex.labels) != num_nodes) 
-    cex.labels = rep(cex.labels, length=num_nodes)
+  if (length(cex.labels) != num_nodes) {
+    cex.labels = rep(cex.labels, length = num_nodes)    
+  }
   cex.labels = cex.labels[aux_ord]
   
   # ======================================================
@@ -195,15 +239,12 @@ arcplot <- function(
   # ======================================================
   # Coordinates of arcs (i.e. edges)
   # ======================================================
-  # 'edgelist' in character format or numeric format?
-#  if (mode(edgelist) == "numeric") tmp = nums else tmp = nodes
-  tmp = nodes
   # handy matrix with numeric indices '1:FROM' , '2:TO'
   e_num = matrix(0, nrow(edgelist), 2)
-  for (i in 1:num_edges)
+  for (i in 1L:num_edges)
   {
-    e_num[i,1] = centers[which(tmp == edgelist[i,1])]
-    e_num[i,2] = centers[which(tmp == edgelist[i,2])]
+    e_num[i,1] = centers[which(nodes == edgelist[i,1])]
+    e_num[i,2] = centers[which(nodes == edgelist[i,2])]
   }    
   # maximum arc radius
   radios = abs(e_num[,1] - e_num[,2]) / 2
@@ -213,71 +254,55 @@ arcplot <- function(
   locs = rowSums(e_num) / 2
   
   # ======================================================
-  # Plot arc diagram horizontally
+  # Plot arc diagram (horizontally or vertically)
   # ======================================================
-  if (horizontal)
-  {
-    # open empty plot window
-    plot(0.5, 0.5, xlim=c(-0.015, 1.015), ylim=c(-0.01, 1*max_rad*2),
-         type="n", xlab="", ylab="", axes=axes, ...)
-    # auxiliar variable for plotting arcs
-    z = seq(0, pi, l=100)
-    # for each edge
-    for (i in 1:num_edges)
-    {
-      # get radius length
-      radio = radios[i]
-      # x-y coords of each arc
-      x = locs[i] + radio * cos(z)
-      y = radio * sin(z)
-      # plot arc connecting nodes
-      lines(x, y, col=col.arcs[i], lwd=lwd.arcs[i], lty=lty,
-            lend=lend, ljoin=ljoin, lmitre=lmitre)
-    }
-    # add node symbols with points
-    if (show.nodes) {
-      points(x=centers, y=rep(0,num_nodes), pch=pch.nodes, 
-             col=col.nodes, bg=bg.nodes, cex=cex.nodes, lwd=lwd.nodes)    
-    }
-    # add node names with mtext
-    if (show.labels) {
-      mtext(labels, side=1, line=line, at=centers, cex=cex.labels, outer=outer,
-            col=col.labels, las=las, font=font, adj=adj, padj=padj, ...)    
-    }    
+  if (horizontal) {
+    side = 1
+    xlim = c(-0.015, 1.015)
+    ylim = c(-0.01, max_rad * 2)
+    x_nodes = centers
+    y_nodes = rep(0, num_nodes)
+    
+  } else {
+    side = 2
+    xlim = c(-0.01, max_rad * 2)
+    ylim = c(-0.015, 1.015)
+    x_nodes = rep(0, num_nodes)
+    y_nodes = centers
   }
   
-  # ======================================================
-  # Plot arc diagram vertically
-  # ======================================================
-  if (!horizontal)
+  # auxiliar variable for plotting arcs
+  z = seq(0, pi, length.out = 100)
+  
+  # open empty plot window
+  plot(0.5, 0.5, xlim=xlim, ylim=ylim,
+       type="n", xlab="", ylab="", axes=axes, ...)
+  # for each edge
+  for (i in 1L:num_edges)
   {
-    # open empty plot window
-    plot(0.5, 0.5, ylim=c(-0.015, 1.015), xlim=c(-0.01, 1*max_rad*2),
-         type="n", xlab="", ylab="", axes=axes, ...)
-    # auxiliar variable for plotting arcs
-    z = seq(0, pi, l=100)
-    # for each edge
-    for (i in 1:num_edges)
-    {
-      # get radius length
-      radio = radios[i]
-      # x-y coords of each arc
-      y = locs[i] + radio * cos(z)
-      x = radio * sin(z)
-      # plot arc connecting nodes
-      lines(x, y, col=col.arcs[i], lwd=lwd.arcs[i], lty=lty,
-            lend=lend, ljoin=ljoin, lmitre=lmitre)
+    # get radius length
+    radio = radios[i]
+    # x-y coords of each arc
+    if (horizontal) {
+      x_arc = locs[i] + radio * cos(z)
+      y_arc = radio * sin(z)      
+    } else {
+      x_arc = radio * sin(z)      
+      y_arc = locs[i] + radio * cos(z)
     }
-    # add node symbols with points
-    if (show.nodes) {
-      points(x=rep(0,num_nodes), y=centers, pch=pch.nodes, 
-             col=col.nodes, bg=bg.nodes, cex=cex.nodes, lwd=lwd.nodes)    
-    }
-    # add node labels with mtext
-    if (show.labels) {
-      mtext(labels, side=2, line=line, at=centers, cex=cex.labels, outer=outer,
-            col=col.labels, las=las, font=font, adj=adj, padj=padj, ...)    
-    }
+    # plot arc connecting nodes
+    lines(x_arc, y_arc, col=col.arcs[i], lwd=lwd.arcs[i], lty=lty,
+          lend=lend, ljoin=ljoin, lmitre=lmitre)
+  }
+  # add node symbols with points
+  if (show.nodes) {
+    points(x=x_nodes, y=y_nodes, pch=pch.nodes, 
+           col=col.nodes, bg=bg.nodes, cex=cex.nodes, lwd=lwd.nodes)    
+  }
+  # add node labels with mtext
+  if (show.labels) {
+    mtext(labels, side=side, line=line, at=centers, cex=cex.labels, outer=outer,
+          col=col.labels, las=las, font=font, adj=adj, padj=padj, ...)    
   }
 
   # --------------------------------
