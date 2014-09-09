dd7125b6a1f317a2f744e8bf87f3794b517ff389
holmrenser <rens.holmer@wur.nl>
2014-09-09 16:21:25 +0200

    Update arcplot.r

--- R/arcplot.r
+++ R/arcplot.r
@@ -473,7 +473,7 @@ arcplot <- function(
   # auxiliar vector for plotting arcs
   z = seq(0, pi, length.out = 100)
   
-  if (!exists("xlim")|!exists("ylim"){
+  if (!exists("xlim")|!exists("ylim")){
      if (horizontal) {
       side = 1
       xlim = c(-0.015, 1.015)
